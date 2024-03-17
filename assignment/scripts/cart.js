40c31184d3647db09b569a661f24ce26c6c8b554
Mustafe Hassan <mustafeh123@gmail.com>
2024-03-17 17:27:08 -0500

    Created global variable for  as empty array,addItem function returns true, loops over  array and logs each item

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,7 +1,18 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket=[];
+function addItem(item){
+    basket.push(item);
+    return true;
+}
+console.log(basket);
 
+function listItems(){
+for(let i=0;i<basket.length;i++){
+    console.log(basket[i]);
+}
+}
 
 
 
